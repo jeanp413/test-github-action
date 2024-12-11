797e136415e7d71bfb0a58baccd34e7287d0b318
Jean Pierre <jeanp413@hotmail.com>
2024-12-11 13:46:54 +0100

    Fix

--- scripts/foo.sh
+++ scripts/foo.sh
@@ -1,8 +1,5 @@
 # use busybox to remove a dependency on image
 ARCH="amd64"
-if [ "$(uname -m)" = "aarch64" ] || [ "$(uname -m)" = "arm64" ]; then
-    ARCH="arm64"
-fi
 AGENT_DOWNLOAD_URL="https://gitpod-flex-releases.s3.amazonaws.com/vscode/development/jpindustrial-muskox/vscode-agent-$ARCH"
 AGENT_INSTALL_DIR="/usr/local/gitpod/shared/vscode/0.1.2024120801"
 AGENT_INSTALL_PATH="$AGENT_INSTALL_DIR/vscode-agent"
